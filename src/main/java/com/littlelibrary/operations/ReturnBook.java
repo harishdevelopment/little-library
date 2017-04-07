49eb44690d6cd75e8423ae826e32211a52cfd846
Harish Kumar <harishkumar.ie@outlook.com>
2017-04-07 17:49:36 +0100

    Dummy Fine Calculation

--- src/main/java/com/littlelibrary/operations/ReturnBook.java
+++ src/main/java/com/littlelibrary/operations/ReturnBook.java
@@ -15,5 +15,16 @@ public class ReturnBook implements Operations {
     public void performOperation(LibraryRepository libraryRepositoryImpl) {
         libraryRepositoryImpl.updateBookStatus(bookTitle, BookStatus.AVAILABLE);
         System.out.println(bookTitle + " has been returned");
+
+        // Store the fine in the repository against the user
+        calculateAndStoreFine(libraryRepositoryImpl);
+    }
+
+    /*
+      This method is dummy as we are not tracking the book issue dates
+      Detail implementation will require detailed repository model
+     */
+    private void calculateAndStoreFine(LibraryRepository libraryRepositoryImpl) {
+        System.out.println("Fine calculated when books are returned");
     }
 }
